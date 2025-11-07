01172a70abeffb6c6ffa19f155a5e2cd85499064
lingfeng <3374080053@qq.com>
2025-11-07 15:14:07 +0800

    Initial Commit

---
+++ Paletclip/PaletclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletclipApp.swift
+//  Paletclip
+//
+//  Created by 凌峰 on 2025/11/7.
+//
+
+import SwiftUI
+
+@main
+struct PaletclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
